f1ffe109607bc9b506296108b08d7a5d2e80992e
Angelique Abacajan <ma_utsey@yahoo.com>
2022-11-01 11:08:12 -0400

    Implemented Access Modifiers mini challenge instruction

---
+++ src/accessModifiers/Accessible.java
@@ -0,0 +1,17 @@
+package accessModifiers;
+
+// Challenge:
+// In the following interface declaration, what is the visibility of:
+//
+// 1. the Accessible interface?
+// 2. the int variable SOME_CONSTANT?
+// 3. methodA?
+// 4. methodB and methodC?
+//
+// Hint: think back to the lecture on interfaces before answering.
+
+public interface Accessible {
+
+
+
+}
